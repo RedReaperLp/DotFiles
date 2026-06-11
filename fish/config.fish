442b14db5621f22616226b12b7563b58df8509a0
Raphael Kuschmierz <fiffykuschmierz@gmail.com>
2026-06-11 16:20:55 +0200

    A

--- fish/config.fish
+++ fish/config.fish
@@ -109,3 +109,4 @@ alias zi=__zoxide_zi
 
 alias sync="~/dotfiles/sync_dotfiles.sh --sync"
 alias vpn='tmux new -s vpn "sudo openvpn --config ~/.config/openvpn/uni_3.ovpn --auth-user-pass ~/.config/openvpn/pass.txt"'
+alias cmod='chmod +x '
